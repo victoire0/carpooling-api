e8433b402aec2af093920237a19b5fd6964da9b2
victoire0 <aminikayembe1@gmail.com>
2025-05-15 04:57:18 +0200

    ajout entite DocumentType

---
+++ src/main/java/com/carpooling/api/entity/DocumentType.java
@@ -0,0 +1,10 @@
+package com.carpooling.api.entity;
+
+public enum DocumentType {
+    DRIVERS_LICENSE,
+    VEHICLE_REGISTRATION,
+    VEHICLE_INSURANCE,
+    ID_CARD,
+    PROFILE_PICTURE,
+    OTHER
+} 
