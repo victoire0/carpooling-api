842f968a5f9827fe9ca20713ab71e53f46da5569
victoire0 <aminikayembe1@gmail.com>
2025-05-16 00:33:55 +0200

    ajout FeedbackRepository

---
+++ src/main/java/com/carpooling/api/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package com.carpooling.api.repository;
+
+import com.carpooling.api.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+} 
