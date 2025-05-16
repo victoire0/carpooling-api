3088093d7887e1b99e9f2efdd7e0edecedfc2365
victoire0 <aminikayembe1@gmail.com>
2025-05-16 08:35:13 +0200

    ajout OpenApiConfig

---
+++ src/main/java/com/carpooling/api/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.carpooling.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${server.port:8080}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Carpooling API")
+                        .version("1.0")
+                        .description("API for the Carpooling application")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server().url("http://localhost:" + serverPort).description("Local server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+} 
